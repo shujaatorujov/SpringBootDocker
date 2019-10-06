93a2b98ae76f3d08ba029f0061013e2310641665
sucu.orucov <sucu.orucov@gmail.com>
2019-10-06 19:44:21 +0400

    Modified

--- src/main/java/com/sucu/SpringBoot/controller/HelloController.java
+++ src/main/java/com/sucu/SpringBoot/controller/HelloController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
+    @GetMapping(path = "/")
+    public String getWelcome(){
+        return "Welcome to Everyone";
+    }
+
     @GetMapping(path = "/hello")
     public String getHello(){
         return "Hello, World";
     }
 
-    @GetMapping(path = "/fazil")
-    public String getFazil(){
-        return "Fazil";
-    }
 }
